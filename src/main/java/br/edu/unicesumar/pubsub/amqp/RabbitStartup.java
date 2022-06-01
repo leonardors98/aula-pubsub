b91fb25cd739e1bf3499eb03c29944fead512dea
leonardo <leonardo.rs.2105@gmail.com>
2022-06-01 22:58:53 +0000

    .

--- src/main/java/br/edu/unicesumar/pubsub/amqp/RabbitStartup.java
+++ src/main/java/br/edu/unicesumar/pubsub/amqp/RabbitStartup.java
@@ -26,9 +26,43 @@ public class RabbitStartup {
     private RabbitTemplate rabbitTemplate;
 
     @PostConstruct
-    private void adiciona() {
+    private void adicionaTopic() {
+        Queue filaTopic = new Queue("fila-Topic", true, false, false);
+        Queue filaDirect = new Queue("fila-Direct", true, false, false);        
+        Queue filaFanout = new Queue("fila-Fanout", true, false, false);
 
-        Queue filaLogInfo = new Queue("info", true, false, false);
+        TopicExchange topicExchange = new TopicExchange("topic-exchange",true,false);
+        Binding bindingFilafilaTopic = new Binding(filaTopic.getName(), DestinationType.QUEUE, topicExchange.getName(), "#.topic.#", null);
+
+    
+        DirectExchange directExchangeTeste = new DirectExchange("direct-exchange-teste", true, false);
+        Binding bindingDirectTeste = new Binding(filaDirect.getName(), DestinationType.QUEUE, directExchangeTeste.getName(), "leo", null);
+        
+
+        FanoutExchange fanoutExchangeTeste = new FanoutExchange("fanout-exchange-teste", true, false);
+        Binding bindingTeste = new Binding(filaFanout.getName(), DestinationType.QUEUE, fanoutExchangeTeste.getName(), "", null);
+        Binding bindingTesteFanout = new Binding(filaDirect.getName(), DestinationType.QUEUE, fanoutExchangeTeste.getName(), "", null);
+        
+        
+        
+        
+        this.amqpAdmin.declareQueue(filaDirect);
+        this.amqpAdmin.declareQueue(filaTopic);
+        this.amqpAdmin.declareQueue(filaFanout);
+        this.amqpAdmin.declareExchange(directExchangeTeste);
+        this.amqpAdmin.declareExchange(topicExchange);
+        this.amqpAdmin.declareExchange(fanoutExchangeTeste);
+        this.amqpAdmin.declareBinding(bindingDirectTeste);
+        this.amqpAdmin.declareBinding(bindingTeste);
+        this.amqpAdmin.declareBinding(bindingTesteFanout);
+        this.amqpAdmin.declareBinding(bindingFilafilaTopic);
+
+        this.rabbitTemplate.convertAndSend(topicExchange.getName(), "topic", "message");
+        this.rabbitTemplate.convertAndSend(directExchangeTeste.getName(), "leo", "message");
+        this.rabbitTemplate.convertAndSend(fanoutExchangeTeste.getName(), "", "message");
+
+
+        /*Queue filaLogInfo = new Queue("info", true, false, false);
         Queue filaLogWarn = new Queue("warn", true, false, false);
         Queue filaLogError = new Queue("error", true, false, false);
         Queue filaLog = new Queue("log-geral", true, false, false);
@@ -73,7 +107,7 @@ public class RabbitStartup {
         //this.amqpAdmin.declareBinding(bindingTeste);
 
         //this.rabbitTemplate.convertAndSend(fanoutExchangeTeste.getName(), "", "message");
-
+*/
     }
 
    /* @RabbitListener(queues = "fila-teste", ackMode = "AUTO")
