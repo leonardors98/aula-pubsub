9ad62ca3361642a8091d5d617324ade9b20fda02
leonardo <leonardo.rs.2105@gmail.com>
2022-07-13 20:58:23 +0000

    hgjf

--- src/main/java/br/edu/unicesumar/pubsub/amqp/RabbitStartup.java
+++ src/main/java/br/edu/unicesumar/pubsub/amqp/RabbitStartup.java
@@ -22,10 +22,41 @@ public class RabbitStartup {
     @Autowired
     private AmqpAdmin amqpAdmin;
 
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
 
     @PostConstruct
     private void postConstruct() {
-        Queue myQueue = new Queue(Message.myUser, true, false, false);
+        Queue filaProvaProgramacao = new Queue("fila-prova-programacao",true,false,false);
+        Queue filaProvaArquitetura = new Queue("fila-prova-arquitetura", true, false,false);
+        Queue filaProva= new Queue("fila-prova",true,false,false);
+
+        FanoutExchange exchangeFanoutProva = new FanoutExchange("exchange-fanout-prova", true, false);
+        DirectExchange exchangeDirectProva = new DirectExchange("exchange-direct-prova", true, false);
+        TopicExchange exchangeTopicProva = new TopicExchange("exchange-topic-prova", true, false);
+
+        Binding bindingDirectProvaQueueProgramacao = new Binding(filaProvaProgramacao.getName(), DestinationType.QUEUE, exchangeDirectProva.getName(), "arquitetura", null);
+        Binding bindingDirectProvaQueueArquitetura = new Binding(filaProvaArquitetura.getName(), DestinationType.QUEUE, exchangeDirectProva.getName(), "programacao", null);
+        Binding bindingTopicProvaQueueProgramacao = new Binding(filaProvaProgramacao.getName(), DestinationType.QUEUE, exchangeTopicProva.getName(), "*.programacao.#", null);
+        Binding bindingTopicProvaQueueArquitetura = new Binding(filaProvaArquitetura.getName(), DestinationType.QUEUE, exchangeTopicProva.getName(), "*.arquitetura.#", null);
+        Binding bindingTopicProvaQueueProva = new Binding(filaProva.getName(), DestinationType.QUEUE, exchangeTopicProva.getName(), "prova.#", null);
+        Binding bindingFanoutProvaQueueProva = new Binding(filaProva.getName(), DestinationType.QUEUE, exchangeFanoutProva.getName(), "", null);
+
+        this.amqpAdmin.declareQueue(filaProvaProgramacao);
+        this.amqpAdmin.declareQueue(filaProvaArquitetura);
+        this.amqpAdmin.declareQueue(filaProva);
+        this.amqpAdmin.declareExchange(exchangeFanoutProva);
+        this.amqpAdmin.declareExchange(exchangeDirectProva);
+        this.amqpAdmin.declareExchange(exchangeTopicProva);
+        this.amqpAdmin.declareBinding(bindingDirectProvaQueueProgramacao);
+        this.amqpAdmin.declareBinding(bindingDirectProvaQueueArquitetura);
+        this.amqpAdmin.declareBinding(bindingTopicProvaQueueProgramacao);
+        this.amqpAdmin.declareBinding(bindingTopicProvaQueueArquitetura);
+        this.amqpAdmin.declareBinding(bindingTopicProvaQueueProva);
+        this.amqpAdmin.declareBinding(bindingFanoutProvaQueueProva);
+
+
+        /*Queue myQueue = new Queue(Message.myUser, true, false, false);
         DirectExchange msgDirect = new DirectExchange("msg-direct", true, false);
         Binding bindingMsgDirectMyQueue = new Binding(Message.myUser, DestinationType.QUEUE, msgDirect.getName(), Message.myUser, null);
 
@@ -33,15 +64,9 @@ public class RabbitStartup {
         this.amqpAdmin.declareQueue(myQueue);
         this.amqpAdmin.declareExchange(msgDirect);
         this.amqpAdmin.declareBinding(bindingMsgDirectMyQueue);
-
-        
-    }
-
-       /* 
-        @Autowired
-    private RabbitTemplate rabbitTemplate;
-       
-       Queue filaLogInfo = new Queue("info", true, false, false);
+        */
+        /* 
+        Queue filaLogInfo = new Queue("info", true, false, false);
         Queue filaLogWarn = new Queue("warn", true, false, false);
         Queue filaLogError = new Queue("error", true, false, false);
         Queue filaLog = new Queue("log-geral", true, false, false);
@@ -70,23 +95,27 @@ public class RabbitStartup {
         // . -> delimitador
 
 
-       // Queue filaTeste = new Queue("fila-teste", true, false, false);
-       // DirectExchange directExchangeTeste = new DirectExchange("direct-exchange-teste", true, false);
-       // Binding bindingTeste = new Binding(filaTeste.getName(), DestinationType.QUEUE, directExchangeTeste.getName(), "igor.gorini", null);
-       // this.amqpAdmin.declareQueue(filaTeste);
-       // this.amqpAdmin.declareExchange(directExchangeTeste);
-       // this.amqpAdmin.declareBinding(bindingTeste);
-       // this.rabbitTemplate.convertAndSend(directExchangeTeste.getName(), "igor.gorini2", "message");
+       Queue filaTeste = new Queue("fila-teste", true, false, false);
+       DirectExchange directExchangeTeste = new DirectExchange("direct-exchange-teste", true, false);
+       Binding bindingTeste = new Binding(filaTeste.getName(), DestinationType.QUEUE, directExchangeTeste.getName(), "igor.gorini", null);
+       this.amqpAdmin.declareQueue(filaTeste);
+       this.amqpAdmin.declareExchange(directExchangeTeste);
+       this.amqpAdmin.declareBinding(bindingTeste);
+       this.rabbitTemplate.convertAndSend(directExchangeTeste.getName(), "igor.gorini2", "message");
 
 
-        //FanoutExchange fanoutExchangeTeste = new FanoutExchange("fanout-exchange-teste", true, false);
-        //Binding bindingTeste = new Binding(filaTeste.getName(), DestinationType.QUEUE, fanoutExchangeTeste.getName(), "", null);
-        //this.amqpAdmin.declareQueue(filaTeste);
-        //this.amqpAdmin.declareExchange(fanoutExchangeTeste);
-        //this.amqpAdmin.declareBinding(bindingTeste);
+       FanoutExchange fanoutExchangeTeste = new FanoutExchange("fanout-exchange-teste", true, false);
+       Binding bindingTeste = new Binding(filaTeste.getName(), DestinationType.QUEUE, fanoutExchangeTeste.getName(), "", null);
+       this.amqpAdmin.declareQueue(filaTeste);
+       this.amqpAdmin.declareExchange(fanoutExchangeTeste);
+       this.amqpAdmin.declareBinding(bindingTeste);
 
-        //this.rabbitTemplate.convertAndSend(fanoutExchangeTeste.getName(), "", "message");
- @RabbitListener(queues = "fila-teste", ackMode = "AUTO")
+       this.rabbitTemplate.convertAndSend(fanoutExchangeTeste.getName(), "", "message");
+*/
+    }
+
+    /* 
+    @RabbitListener(queues = "fila-teste", ackMode = "AUTO")
     private void consumerFilaTeste(Aluno aluno) {
         try {
             Thread.sleep(15000);
@@ -94,6 +123,7 @@ public class RabbitStartup {
             e.printStackTrace();
         }
         System.out.println(aluno);
-    }*/
+    }
 
+    */
 }
